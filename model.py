ca7c842528c3b1722e58990d959953df195f208d
Andre Montgomery <andremontgomery166@gmail.com>
2024-05-06 15:06:21 -0700

    oops

--- model.py
+++ model.py
@@ -3,61 +3,62 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     username = db.Column(db.String(50), unique = True, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
     password = db.Column(db.String(128), nullable = False) 
-    screen_name = db.Column(db.String(50), nullable = False) #If user wants to have diff screen name
     profile_pic = db.Column(db.String(100))
-    # allergies = db.Column(db.String(255)) make its own table
-    # favorites = db.relationship('Recipe', secondary=favorites, lazy='subquery',
-    #                             backref=db.backref('users', lazy=True))
+
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.query.filter(User.username == username).first()
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.screen_name}')"
+        return f"User('{self.username}', '{self.email}')"
 
-class Allergies(db.Model):
+class Allergy(db.Model):
     __tablename__ = "allergies"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.Text)
-    user = db.relationship("User", back_populates="allergies")
-    def __repr__(self):
-        return f"Allergies('{self.name}', '{self.description}')"
-        
-class Quantity(db.Model):
-    __tablename__ = "quantity"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.text, nullable=False)
-    type = db.relationship('Type', backref='quantity' )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    ingredient_name = db.Column(db.String, db.ForeignKey('ingredients.name'), nullable=False)
+    user = db.relationship('User', backref=db.backref('allergies', lazy=True))
+    ingredient = db.relationship('Ingredient', backref=db.backref('allergies', lazy=True))
 
     def __repr__(self):
-        return f"Quantity('{self.name}', '{self.description}', '{self.type}')"
-    
+        return f"Allergy('{self.user_id}', '{self.ingredient_name}')"
+
 class Recipe(db.Model):
-    __tablename__ = "recipe"
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "recipes"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
-    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
-    
+    served = db.Column(db.Text)
+    ingredients = db.Column(db.Text)
+    key = db.Column(db.Text)
     def __repr__(self):
-        return f"Recipe('{self.name}', '{self.description}', '{self.ingredients})"
+        return f"Recipe('{self.name}', '{self.description}', '{self.key}, '{self.served}', '{self.ingredients})"
+    
+    @classmethod
+    def create(cls, name, description, served, ingredients, key):
+
+       return cls(name=name, description=description, served=served, ingredients=ingredients, key=key)
 
 class Ingredient(db.Model):
-    __tablename__ = "ingredient"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.String(20))
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    __tablename__ = "ingredients"
+    name = db.Column(db.String(100), nullable=False, primary_key=True)
 
     def __repr__(self):
-        return f"Ingredient('{self.name}', '{self.quantity}')"
+        return f"Ingredient('{self.name}')"
+
+    @classmethod
+    def create(cls, name):
+
+       return cls(name=name)
+
 
 def connect_to_db(app, db_name = 'cocktails'):
-    app.config["SQLACHEMY_DATABASE_URI"]=f"postgresql:///{db_name}"
+    app.config["SQLALCHEMY_DATABASE_URI"]=f"postgresql:///{db_name}"
     app.config["SQLALCHEMY_ECHO"] = True
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
@@ -65,4 +66,5 @@ def connect_to_db(app, db_name = 'cocktails'):
 
 if __name__ == "__main__":
     from server import app
+    app.app_context().push()
     connect_to_db(app)
--- server.py
+++ server.py
@@ -1,40 +1,38 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_login import LoginManager, login_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
-from model import connect_to_db
+from model import connect_to_db, db, Recipe, Ingredient, User, Allergy
+import os
 
 app = Flask(__name__)
-
-# favorites = db.Table('favorites',       #make into a table in model.py
-#     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-#     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
-# )
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-#come back for research
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
-app.secret_key = "dev" #make this secret tomoro with trew
-app.jinja_env.undefined = StrictUndefined
+app.secret_key = os.environ['SECRET_KEY']
+# app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    return redirect(url_for('search'))
+    return render_template("index.html")
+
+@app.route('/search_bar')
+def search_bar():
+    return render_template("search.html")
 
 @app.route('/search', methods=['POST'])
 def search():
     ingredient1 = request.form['ingredient1']
     ingredient2 = request.form['ingredient2']
+    ingredient3 = request.form['ingredient3']
+    ingredient4 = request.form['ingredient4']
+    user_input = {ingredient1.lower(), ingredient2.lower(), ingredient3.lower(), ingredient4.lower()}
 
-    recipes = Recipe.query.join(Ingredient).filter(
-        Ingredient.name == ingredient1).join(
-        Ingredient, Recipe.id == Ingredient.recipe_id).filter(
-        Ingredient.name == ingredient2).all()
+    filtered_recipe = set()
+    recipes = Recipe.query.all() # [ <Recipe>, <Recipe>, <Recipe>.... ] => <Recipe>.key
 
-    return render_template('recipes.html', recipes=recipes)
+    for recipe in recipes:
+        recipe_ingredients = set(recipe.key.strip().lower().split(',')) # iterating over all recipes
+        if recipe_ingredients.intersection(user_input):
+            filtered_recipe.add(recipe)
+
+    return render_template('recipes.html', recipes=filtered_recipe)
 
 @app.route('/favorite/<int:recipe_id>', methods=['POST'])
 @login_required
@@ -45,27 +43,62 @@ def favorite(recipe_id):
             current_user.favorites.append(recipe)
             db.session.commit()
             return 'Recipe favorited successfully!'
-    return 'You must be logged in to favorite recipes.'
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Process user login."""
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = User.get_by_username(username)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        # Log in user by storing the user's email in session
+        session["user_username"] = user.username
+        flash(f"Welcome back, {user.username}!")
+
+    return redirect("/")
+
+@app.route("/login", methods=["GET"])
+def login():
+
+    return render_template("login.html")
+
+@app.route("/register_user")
+def register_user():
+    all_allergies = [allergy.ingredient_name for allergy in Allergy.query.all()]
+    return render_template("register.html", all_allergies=all_allergies)
 
 @app.route('/register', methods = ['POST'])
 def register():
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
-    screen_name = request.form['screen_name']
-    profile_pic = request.form['profilepic', '']
-    allergies = request.form['allergies', '']
+    profile_pic = request.form.get('profilepic', '')
+    allergies = request.form.get('allergies', '')
 
     hashed_password = generate_password_hash(password)
 
     user = User(username=username, email=email, password=hashed_password, 
-                screen_name=screen_name, profile_pic=profile_pic, allergies=allergies)
+                profile_pic=profile_pic)
     db.session.add(user)
     db.session.commit()
+    if allergies:
+        current_boi = User.query.filter(User.username==username).first()
+        allergies_list = allergies.strip().split(',')
+        for allergy in allergies_list:
+            new_allergy = Allergy(user_id = current_boi.id, ingredient_name=allergy)
+            db.session.add(new_allergy)
+    db.session.commit()
+    flash("Account created!")
 
-    return 'Registration successful!'
+    return redirect("/")
 
 
 if __name__ == "__main__":
+    app.app_context().push()
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
